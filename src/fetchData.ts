332575a02f26f40af79f48acf9d4bbf52e6c0822
Evgeniy <epchelinov@gmail.com>
2021-02-19 14:54:04 +1000

    заменил axios на fetch (в firefox не работал mswjs)

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
 import { Data } from "@/data.model";
 
 export default async function fetchData(): Promise<Data | null> {
   try {
-    const response = await axios.get("/api/data");
-    const data: Data = await response.data.results;
+    const response = await fetch("/api/data");
+    const json = await response.json();
+    const data: Data = await json.results;
 
-    return data;
+    return await data;
   } catch (e) {
     console.warn(`getData -> Error: ${e}`);
     return null;
